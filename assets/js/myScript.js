807f890a8e4606fdc845d95f9ef29979069d6707
Hacking-NASSA-with-HTML <follow.trend7777@gmail.com>
2023-01-17 15:55:18 +0400

    feat: add basic styling

--- assets/js/myScript.js
+++ assets/js/myScript.js
@@ -4,20 +4,22 @@ function mainPageContent() {
     const renderMainPageContent =
         `
         <div class="container">
-            <header>Log In or Sign Up to vote</header>
-            <h1>What is more tasty?</h1>
-            <fieldset>
-                <legend>Select your option</legend>
-                <div>
-                    <input type="radio" id="apples" name="vote" value="apples"
-                            checked>
-                    <label for="apples">Apples</label>
-                </div>
-                <div>
-                    <input type="radio" id="oranges" name="vote" value="oranges">
-                    <label for="oranges">Oranges</label>
-                </div>
-            </fieldset>
+            <header class="header">Log In or Sign Up to vote</header>
+            <h1 class="h1">What is more tasty?</h1>
+            <div class="fieldWrapper">
+                <fieldset class="fieldset">
+                    <legend>Select your option</legend>
+                    <div>
+                        <input type="radio" id="apples" name="vote" value="apples"
+                                checked>
+                        <label for="apples">Apples</label>
+                    </div>
+                    <div>
+                        <input type="radio" id="oranges" name="vote" value="oranges">
+                        <label for="oranges">Oranges</label>
+                    </div>
+                </fieldset>
+            </div>
         </div>
         <div class="buttonWrapperDiv">
             <button id="button">Click To See Total Results</button>
