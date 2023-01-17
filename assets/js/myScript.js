4c83a1bd09557ab8f5010335dcccee9da1445c66
Hacking-NASSA-with-HTML <follow.trend7777@gmail.com>
2023-01-17 15:38:11 +0400

    feat: add main HTML skeleton

--- assets/js/myScript.js
+++ assets/js/myScript.js
@@ -1,1 +1,34 @@
-console.log('Hi!')
+const root = document.getElementById('root')
+
+function mainPageContent() {
+    const renderMainPageContent =
+        `
+        <div class="container">
+            <header>Log In or Sign Up to vote</header>
+            <h1>What is more tasty?</h1>
+            <fieldset>
+                <legend>Select your option</legend>
+                <div>
+                    <input type="radio" id="apples" name="vote" value="apples"
+                            checked>
+                    <label for="apples">Apples</label>
+                </div>
+                <div>
+                    <input type="radio" id="oranges" name="vote" value="oranges">
+                    <label for="oranges">Oranges</label>
+                </div>
+            </fieldset>
+        </div>
+        <div class="buttonWrapperDiv">
+            <button id="button">Click To See Total Results</button>
+        </div>
+        <div id="resultsWrapperDiv">
+            <div class="finalResult">Results Are Shown Here</div>
+            <div id="apples">Apples 43%</div>
+            <div id="oranges">Oranges 57%</div>
+        </div>
+    `
+    return renderMainPageContent
+}
+
+root.insertAdjacentHTML("afterend", mainPageContent())
